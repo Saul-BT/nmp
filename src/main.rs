025271541588f949b45c056d60775443f930d142
Saul Blanco Tejero <saul.blanco.tejero@iesjulianmarias.es>
2021-01-08 18:40:29 +0100

    Add clap dependency and cli arguments skeleton

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,23 @@
   LICENSE: GPLv3
 */
 
+use clap::{load_yaml, App};
+use nmp::models::task::{Task, TaskPriority, TaskState};
+use nmp::models::task_stack::TaskStack;
+
 fn main() {
-    println!("Hello, world!");
+    // The YAML file has the cli arguments config
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from(yaml).get_matches();
+
+    //if let Some(c) = matches.value_of("config") {
+    //    println!("Value for config: {}", c);
+    //}
+
+    match matches.subcommand_name() {
+        Some("add") => println!("TODO add"),
+        Some("remove") => println!("TODO remove"),
+        Some("list") => println!("TODO list"),
+        _ => println!("Launching TUI..."),
+    }
 }
